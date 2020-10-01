97fa9d350bbb042f116ded32d067e9640f568d7c
Christianne Marie Luntok <christianne.luntok1@tafensw.net.au>
2020-10-01 21:26:15 +1000

    fixed the navigation

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,17 @@
 // Put your applicaiton javascript here
+var bottomNavbar = document.getElementById("bottom-nav");
+    var sticky = bottomNavbar.offsetTop;
+
+    console.log(bottomNavbar);
+    console.log(sticky);
+
+
+    function stickyHeader (){
+        if (window.pageYOffset >= sticky) {
+            bottomNavbar.classList.add("sticky");
+        } else {
+            bottomNavbar.classList.remove("sticky");
+        }
+    }
+
+    window.onscroll = function(){stickyHeader()};
